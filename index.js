87e38f499eaf430a367841e2caf64725147592cb
Gambia & Sengegal Tours <jboyyai@gmail.com>
2026-08-03 10:30:19 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 4000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 10000,
+    allow_origin: '*'
+  },
+  relay: {
+    ffmpeg: '/usr/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        mode: 'push',
+        edge: 'rtmp://live.twitch.tv/app/live_1238068816_utEriRNJTUAlQSmD4iwQhgjSeGbV3q'
+      }
+    ]
+  }O
+};
+
+var nms = new NodeMediaServer(config);
+nms.run();
